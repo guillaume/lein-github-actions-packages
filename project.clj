12a126ddf10307a06efac33413334dee524ba7a9
Guillaume Carbonneau <guillaume.carbonneau@gmail.com>
2020-06-29 21:43:56 -0400

    change group id

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject github-actions-packages/lein-template "0.0.1"
+(defproject org.clojars.gcarbonneau/github-actions-packages-lein-template "0.0.1"
   :description "A leiningen template for deploying a clojure library to a github packages repo"
   :url "https://github.com/guillaume/github-actions-packages"
   :license {:name "Eclipse"
